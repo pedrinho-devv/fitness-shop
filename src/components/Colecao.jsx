ccae7293bad8e6558a9a86faafa7153c0f9713b9
PedroAugusto <pedrodesousakiske@gmail.com>
2025-01-30 14:14:22 -0300

    problemas no hero img

--- src/components/Colecao.jsx
+++ src/components/Colecao.jsx
@@ -5,11 +5,11 @@ import ProdutoItem from "./ProdutoItem";
 
 const Colecao = () => {
   const { products } = useContext(ShopContext);
-  const [ultimosProdutos, setUltimosProdutos] = useState([])
+  const [ultimosProdutos, setUltimosProdutos] = useState([]);
 
   useEffect(() => {
-    setUltimosProdutos(products.slice(0,10))
-  }, [])
+    setUltimosProdutos(products.slice(0, 10));
+  }, []);
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
@@ -19,14 +19,18 @@ const Colecao = () => {
           debitis accusantium impedit fugiat.
         </p>
       </div>
-        {/*PRODUTOS */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-            {
-                ultimosProdutos.map((item, index) => (
-                    <ProdutoItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-                ))
-            }
-        </div>
+      {/*PRODUTOS */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {ultimosProdutos.map((item, index) => (
+          <ProdutoItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
