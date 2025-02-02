e5dbb30b821978fe5e12fe6e97175ae9038ff0cf
PedroAugusto <pedrodesousakiske@gmail.com>
2025-02-02 10:59:08 -0300

    correção no input desalinhado.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,8 +6,8 @@ const Search = () => {
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
 
     return showSearch ? (
-        <div className="border-t border-b bg-gray-50 text-center py-3">
-            <div className="inline-flex items-center justify-center border border-gray-400 px-5 mx-5 py-2 rounded-full w-full sm:w-1/2">
+        <div className="border-t border-b bg-gray-50 text-center flex justify-center items-center py-3">
+            <div className="inline-flex items-center justify-center border border-gray-400 px-5 mx-5 py-2 rounded-full w-full">
                 <input 
                     value={search} 
                     onChange={(e) => setSearch(e.target.value)} 
